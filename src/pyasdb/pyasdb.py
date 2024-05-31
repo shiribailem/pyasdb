a857a239025043a571f32388c4673f7a3490fbe0
Shiri Bailem <shiri@bailem.me>
2024-05-31 14:47:11 -0500

    Added type checking to ensure table entries are always dictionaries

--- src/pyasdb/pyasdb.py
+++ src/pyasdb/pyasdb.py
@@ -67,6 +67,8 @@ class Query:
         :param key(int): list index of result
         :return:
         """
+        if not isinstance(value, dict):
+            raise TypeError("Value must be a dictionary")
         if isinstance(key, int):
             self.table[self.results[key]] = value
         elif isinstance(key, str):
@@ -129,6 +131,8 @@ class Table:
         :param value: new contents
         :param sync: boolean specifying to immediately sync if in writeback mode
         """
+        if not isinstance(value, dict):
+            raise TypeError("Value must be a dictionary")
         self.parent.shelf['.'.join((self.name, key))] = value
         if sync:
             self.parent.sync()
