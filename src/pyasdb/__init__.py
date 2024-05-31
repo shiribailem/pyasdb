0dcca7c1db780caecc9552568b726eb99e739123
Shiri Bailem <shiri@bailem.me>
2024-05-31 11:36:53 -0500

    Incorporate primary docstring

--- src/pyasdb/__init__.py
+++ src/pyasdb/__init__.py
@@ -1,1 +1,13 @@
+"""
+Python Yet Another Simple Database - A simple offline local pythonic database backed by shelve/pickle.
+
+This package provides the following classes:
+- DB: The main database class.
+- Table: Class for managing individual database tables.
+- Query: Class for making and managing results of queries.
+
+Usage:
+    from pyasdb import DB, Table, Query
+"""
+
 from .pyasdb import DB, Table, Query
