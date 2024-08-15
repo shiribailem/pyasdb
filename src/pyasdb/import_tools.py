aec780704b4ac41d42b71b1f66c51759f1e93dc4
Shiri Bailem <shiri@bailem.me>
2024-08-14 23:34:30 -0500

    enable bypassing locks for bulk uploads

--- src/pyasdb/import_tools.py
+++ src/pyasdb/import_tools.py
@@ -7,36 +7,55 @@ from csv import DictReader
 from ast import literal_eval
 
 
-def csv_import(file, table, index, autoconvert=True):
+def csv_import(file, db, tablename, index, autoconvert=True, debug=False):
+    if debug:
+        print(f'Importing file into db')
     data = DictReader(file)
+    if debug:
+        print(f'file mapped')
+        print("Getting Bulk Lock")
 
-    row: dict
-    for row in data:
-        if index not in row.keys():
-            continue
+    with db.get_bulk_lock():
+        if debug:
+            print("Bulk Lock acquired")
+        table = db[tablename]
+        if debug:
+            print("Table acquired")
 
-        if autoconvert:
-            removekeys = []
-            for key in row:
-                if row[key] == '' or row[key] is None:
-                    removekeys.append(key)
-                else:
-                    if row[key].isdigit():
-                        row[key] = int(row[key])
+        row: dict
+        for row in data:
+            if index not in row.keys():
+                continue
+
+            if debug:
+                backspace = '\b'
+                print(f'{ backspace * 20 } Row: { row[index] }', end='')
+
+            if autoconvert:
+                removekeys = []
+                for key in row:
+                    if row[key] == '' or row[key] is None:
+                        removekeys.append(key)
                     else:
-                        try:
-                            row[key] = float(row[key])
-                        except ValueError:
+                        if row[key].isdigit():
+                            row[key] = int(row[key])
+                        else:
                             try:
-                                row[key] = literal_eval(row[key])
-                            except (ValueError, SyntaxError):
-                                if dateutil:
-                                    try:
-                                        row[key] = dateutil.parser.parse(row[key])
-                                    except:
-                                        pass
+                                row[key] = float(row[key])
+                            except ValueError:
+                                try:
+                                    row[key] = literal_eval(row[key])
+                                except (ValueError, SyntaxError):
+                                    if dateutil:
+                                        try:
+                                            row[key] = dateutil.parser.parse(row[key])
+                                        except:
+                                            pass
 
-            for key in removekeys:
-                row.pop(key)
+                for key in removekeys:
+                    row.pop(key)
 
-        table[str(row[index])] = row
+            table[str(row[index])] = row
+    db.release_bulk_lock()
+    if debug:
+        print("\nBulk Lock released")
