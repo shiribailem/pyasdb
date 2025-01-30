2cdaa0e7d131df1c086b8ab5a83ffd25403f7b56
Shiri Bailem <shiri@bailem.me>
2025-01-30 11:29:09 -0600

    modify behaviors of PickleDBM, don't write file if not needed and perform a swap write to reduce chance of corruption

--- src/pyasdb/backends/pickle.py
+++ src/pyasdb/backends/pickle.py
@@ -21,15 +21,24 @@ class PickleDBM:
         else:
             self.data = dict()
 
+        self.updated = False
+
     def close(self):
         self.sync()
         self.closed = True
         self.data = None
 
     def sync(self):
-        if not self.closed:
-            with open(self.filename, 'wb') as file:
+        if not self.closed and self.updated:
+            with open(self.filename + '_new', 'wb') as file:
                 file.write(pickle.dumps(self.data))
+            # Delete original and swap in new file
+            try:
+                os.remove(self.filename)
+            except FileNotFoundError:
+                # If the file is gone then nothing to worry about
+                pass
+            os.rename(self.filename + '_new', self.filename)
 
     def __getattr__(self, key):
         return getattr(self.data, key)
@@ -43,8 +52,10 @@ class PickleDBM:
         if self.closed:
             raise ValueError('PickleDBM Backend No Longer Open.')
         self.data[key] = value
+        self.updated = True
 
     def __delitem__(self, key):
         if self.closed:
             raise ValueError('PickleDBM Backend No Longer Open.')
         del self.data[key]
+        self.updated = True
