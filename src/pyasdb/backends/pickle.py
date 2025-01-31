bf47a6395fee12515f40f39d7e940a0e191687c8
Shiri Bailem <shiri@bailem.me>
2025-01-30 18:32:51 -0600

    Implemented first journaling routine, TODO move to separate module so it can easily be re-used

--- src/pyasdb/backends/pickle.py
+++ src/pyasdb/backends/pickle.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from hashlib import md5
+from datetime import datetime
 
 
 class PickleDBM:
@@ -36,13 +37,30 @@ class PickleDBM:
             self.data = dict()
 
         self.updated = False
+        self.journal = []
+
+        if os.path.exists(filename + '.journal'):
+            self.recovery()
 
     def close(self):
-        self.sync()
+        self.flush()
         self.closed = True
         self.data = None
 
-    def sync(self):
+    # Updates the journal with all current activities
+    def write(self):
+        if not self.closed and len(self.journal) > 0:
+            with open(self.filename + '.journal', 'ab') as file:
+                for line in self.journal:
+                    data = pickle.dumps(line)
+                    size = len(data).to_bytes(4)
+                    checksum = md5(size+data).digest()
+                    file.write(checksum + size + data)
+                self.journal = []
+
+    # This is for when the journal isn't needed, write everything to the file
+    # and erase the no longer needed journal
+    def flush(self):
         if not self.closed and self.updated:
             data = pickle.dumps(self.data)
             new_checksum = md5(data).hexdigest()
@@ -63,6 +81,35 @@ class PickleDBM:
                     pass
                 os.rename(self.filename + '_new', self.filename)
 
+                # If this point is reached then the data was successfully written
+                # and the journal is no longer needed.
+                if os.path.exists(self.filename + '.journal'):
+                    os.remove(self.filename + '.journal')
+            self.updated = False
+
+    def recovery(self):
+        if os.path.exists(self.filename + '.journal'):
+            with open(self.filename + '.journal', 'rb') as file:
+                while True:
+                    checksum = file.read(16)
+                    if not checksum:
+                        break
+                    size_raw = file.read(4)
+                    size = int.from_bytes(size_raw, 'big')
+                    data = file.read(size)
+                    if checksum != md5(size_raw+data).digest():
+                        raise ValueError(f'MD5SUM MISMATCH')
+                    segment = pickle.loads(data)
+
+                    if segment['action'] == 'set':
+                        self.data[segment['key']] = segment['value']
+                    elif segment['action'] == 'del':
+                        del self.data[segment['key']]
+
+    # Now deprecated routine replaced by write
+    def sync(self):
+        self.write()
+
     def __getattr__(self, key):
         return getattr(self.data, key)
 
@@ -75,10 +122,12 @@ class PickleDBM:
         if self.closed:
             raise ValueError('PickleDBM Backend No Longer Open.')
         self.data[key] = value
+        self.journal.append({'action': 'set', 'key': key, 'value': value, 'time': datetime.now()})
         self.updated = True
 
     def __delitem__(self, key):
         if self.closed:
             raise ValueError('PickleDBM Backend No Longer Open.')
         del self.data[key]
+        self.journal.append({'action': 'del', 'key': key, 'time': datetime.now()})
         self.updated = True
